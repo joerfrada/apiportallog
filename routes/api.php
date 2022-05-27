5a83c790e34cfefdff8f4f8b2c2592776e0f8066
Joe Ronald Flórez Rada <joerfrada@gmail.com>
2022-05-27 00:52:42 -0500

    Cambios 27/05/2022

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AplicacionController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MenuController;
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\UsuarioMenuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,19 +29,4 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('/aplicacion/crearAplicaciones', [AplicacionController::class, 'crearAplicaciones']);
     Route::post('/aplicacion/actualizarAplicaciones', [AplicacionController::class, 'actualizarAplicaciones']);
     Route::get('/aplicacion/getTipoAplicaciones', [AplicacionController::class, 'getTipoAplicaciones']);
-
-    // Menu
-    // Route::post('/menu/getMenus', [MenuController::class, 'getMenus']);
-    // Route::post('/menu/crearMenus', [MenuController::class, 'crearMenus']);
-    // Route::post('/menu/actualizarMenus', [MenuController::class, 'actualizarMenus']);
-
-    // Usuarios
-    Route::get('/usuario/getUsuariosFull', [UsuarioController::class, 'getUsuariosFull']);
-    Route::post('/usuario/getUsuarios', [UsuarioController::class, 'getUsuarios']);
-    Route::post('/usuario/crearUsuarios', [UsuarioController::class, 'crearUsuarios']);
-    Route::post('/usuario/actualizarUsuarios', [UsuarioController::class, 'actualizarUsuarios']);
-
-    // Usuarios-Menu
-    // Route::post('/usuariomenu/crearUsuarioMenu', [UsuarioMenuController::class, 'crearUsuarioMenu']);
-    // Route::post('/usuariomenu/actualizarUsuarioMenu', [UsuarioMenuController::class, 'actualizarUsuarioMenu']);
 });
